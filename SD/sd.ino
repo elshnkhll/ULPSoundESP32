8e1f4f9486838317d87beda0b6a2fc0535763eae
ELSHNKHLL <elshnkhll@yahoo.com>
2020-11-19 16:09:49 -0500

    Update sd.ino

--- SD/sd.ino
+++ SD/sd.ino
@@ -3,25 +3,19 @@
 #include <driver/dac.h>
 #include <soc/rtc.h>
 #include <math.h>
-
 #include "SD.h"
 #include "SPI.h"
+
 SPIClass sdSPI(VSPI);
 
-#define SDCARD_SS       13
-#define SDCARD_CLK      18
-#define SDCARD_MOSI     23
-#define SDCARD_MISO     2
-
-
+#define SDCARD_SS 13
+#define SDCARD_CLK 18
+#define SDCARD_MOSI 23
+#define SDCARD_MISO 2
 
 #include <FS.h>
 
 // Connect speaker on pin 25 and SD card in MMC mode
-
-// Convert your mp3/acc/flac/wav ... with :
-// https://bitluni.net/wp-content/uploads/2018/03/WavetableEditor.html
-// or use audacity and an hexadecimal editor (like HxD).
 
 unsigned long samplingRate = 11025;
 const int opcodeCount = 17;
@@ -40,24 +34,20 @@ byte Current_Buffer = 1;
 
 File file;
 
-void startULPSound()
-{
+void startULPSound() {
   pinMode(2, INPUT_PULLUP);
-
-
-
-
 
   sdSPI.begin(SDCARD_CLK, SDCARD_MISO, SDCARD_MOSI, SDCARD_SS);
   if (!SD.begin(SDCARD_SS, sdSPI)) {
 
-
     Serial.println(F("Cannot init SD"));
   }
 
+  // sample is 11025 Hz 8 bit PCM in .WAV container
+  // dowloaded from http://www.nch.com.au/acm/11k8bitpcm.wav
+
   file = SD.open("/11k8bitpcm.wav");
-  if (!file)
-  {
+  if (!file) {
     Serial.println(F("cannot open file"));
   }
   file.seek(0);
@@ -65,7 +55,7 @@ void startULPSound()
   Serial.println(F("Calc ULP Clock"));
   //calculate the actual ULP clock
   unsigned long rtc_8md256_period = rtc_clk_cal(RTC_CAL_8MD256, 1000);
-  unsigned long rtc_fast_freq_hz = 1000000ULL * (1 << RTC_CLK_CAL_FRACT) * 256 / rtc_8md256_period;
+  unsigned long rtc_fast_freq_hz = 1000000 ULL * (1 << RTC_CLK_CAL_FRACT) * 256 / rtc_8md256_period;
 
   Serial.println(F("Init dac"));
   //initialize DACs
@@ -88,52 +78,51 @@ void startULPSound()
     //reset offset register
     I_MOVI(R3, 0),
     //delay to get the right sampling rate
-    I_DELAY(dt),                // 6 + dt
+    I_DELAY(dt), // 6 + dt
     //reset sample index
-    I_MOVI(R0, 0),              // 6
+    I_MOVI(R0, 0), // 6
     //write the index back to memory for the main cpu
     I_ST(R0, R3, indexAddress), // 8
     //divide index by two since we store two samples in each dword
-    I_RSHI(R2, R0, 1),          // 6
+    I_RSHI(R2, R0, 1), // 6
     //load the samples
-    I_LD(R1, R2, bufferStart),  // 8
+    I_LD(R1, R2, bufferStart), // 8
     //get if odd or even sample
-    I_ANDI(R2, R0, 1),          // 6
+    I_ANDI(R2, R0, 1), // 6
     //multiply by 8
-    I_LSHI(R2, R2, 3),          // 6
+    I_LSHI(R2, R2, 3), // 6
     //shift the bits to have the right sample in the lower 8 bits
-    I_RSHR(R1, R1, R2),         // 6
+    I_RSHR(R1, R1, R2), // 6
     //mask the lower 8 bits
-    I_ANDI(R1, R1, 255),        // 6
+    I_ANDI(R1, R1, 255), // 6
     //multiply by 2
-    I_LSHI(R1, R1, 1),          // 6
+    I_LSHI(R1, R1, 1), // 6
     //add start position
-    I_ADDI(R1, R1, dacTableStart1),// 6
+    I_ADDI(R1, R1, dacTableStart1), // 6
     //jump to the dac opcode
-    I_BXR(R1),                  // 4
+    I_BXR(R1), // 4
     //here we get back from writing a sample
     //increment the sample index
-    I_ADDI(R0, R0, 1),          // 6
+    I_ADDI(R0, R0, 1), // 6
     //if reached end of the buffer, jump relative to index reset
-    I_BGE(-13, totalSamples),   // 4
+    I_BGE(-13, totalSamples), // 4
     //wait to get the right sample rate (2 cycles more to compensate the index reset)
-    I_DELAY((unsigned int)dt + 2),            // 8 + dt
+    I_DELAY((unsigned int) dt + 2), // 8 + dt
     //if not, jump absolute to where index is written to memory
     I_BXI(3)
-  };                  // 4
+  }; // 4
   // write io and jump back another 12 + 4
 
   Serial.println(F("Load ULP"));
   size_t load_addr = 0;
   size_t size = sizeof(mono) / sizeof(ulp_insn_t);
-  ulp_process_macros_and_load(load_addr, mono, &size);
+  ulp_process_macros_and_load(load_addr, mono, & size);
   //  this is how to get the opcodes
   //  for(int i = 0; i < size; i++)
   //    Serial.println(RTC_SLOW_MEM[i], HEX);
 
   //create DAC opcode tables
-  for (int i = 0; i < 256; i++)
-  {
+  for (int i = 0; i < 256; i++) {
     RTC_SLOW_MEM[dacTableStart1 + i * 2] = 0x1D4C0121 | (i << 10); //dac0
     RTC_SLOW_MEM[dacTableStart1 + 1 + i * 2] = 0x80000000 + retAddress1 * 4;
   }
@@ -155,52 +144,42 @@ long currentSample = 0;
 
 long pos = 0;
 
-unsigned char nextSample()
-{
-  return (unsigned char)((int)Buffer[Current_Buffer][pos++]); //+128
+unsigned char nextSample() {
+  return (unsigned char)((int) Buffer[Current_Buffer][pos++]); //+128
 }
 
 int lastFilledWord = 0;
 
-void Fill_Buffer(void *pvParameters)
-{
+void Fill_Buffer(void * pvParameters) {
   Serial.println(F("fill buffer"));
-  (void)pvParameters;
+  (void) pvParameters;
   for (;;) {
-    if (Current_Buffer == 1)
-    {
-      file.read((byte *)Buffer[0], totalSampleWords * 2);
-    }
-    else {
-      file.read((byte *)Buffer[1], totalSampleWords * 2);
+    if (Current_Buffer == 1) {
+      file.read((byte * ) Buffer[0], totalSampleWords * 2);
+    } else {
+      file.read((byte * ) Buffer[1], totalSampleWords * 2);
     }
     vTaskSuspend(NULL);
   }
 }
 
-void fillSamples(void *pvParameters)
-{
-  (void)pvParameters;
+void fillSamples(void * pvParameters) {
+  (void) pvParameters;
   Serial.println(F("fillSamples"));
   for (;;) {
     int currentSample = RTC_SLOW_MEM[indexAddress] & 0xffff;
     int currentWord = currentSample >> 1;
-    while (lastFilledWord != currentWord)
-    {
+    while (lastFilledWord != currentWord) {
       unsigned int w = nextSample();
       w |= nextSample() << 8;
       RTC_SLOW_MEM[bufferStart + lastFilledWord] = w;
       lastFilledWord++;
-      if (lastFilledWord == totalSampleWords)
-      {
+      if (lastFilledWord == totalSampleWords) {
         lastFilledWord = 0;
         pos = 0;
-        if (Current_Buffer == 1)
-        {
+        if (Current_Buffer == 1) {
           Current_Buffer = 0;
-        }
-        else
-        {
+        } else {
           Current_Buffer = 1;
         }
         vTaskResume(Fill_Buffer_Handle);
@@ -210,20 +189,18 @@ void fillSamples(void *pvParameters)
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.print("Total samples: ");
   Serial.println(totalSamples);
   startULPSound();
 
-  xTaskCreatePinnedToCore(Fill_Buffer, "Fill Buffer", 1024 * 6, NULL, 2, &Fill_Buffer_Handle, 1);
+  xTaskCreatePinnedToCore(Fill_Buffer, "Fill Buffer", 1024 * 6, NULL, 2, & Fill_Buffer_Handle, 1);
   Current_Buffer = 0;
   xTaskCreatePinnedToCore(fillSamples, "Fill Sample", 1024 * 6, NULL, 2, NULL, 0);
 
 }
 
-void loop()
-{
+void loop() {
   vTaskDelete(NULL);
 }
