7bfdbdbc241c8bbf95139246b8111ff45d646b2e
ELSHNKHLL <elshnkhll@yahoo.com>
2020-11-24 11:46:43 -0500

    Update sd.ino
    Stop repeating last buffer.

--- SD/sd.ino
+++ SD/sd.ino
@@ -3,15 +3,15 @@
 #include <driver/dac.h>
 #include <soc/rtc.h>
 #include <math.h>
+
 #include "SD.h"
 #include "SPI.h"
-
 SPIClass sdSPI(VSPI);
 
-#define SDCARD_SS 13
-#define SDCARD_CLK 18
-#define SDCARD_MOSI 23
-#define SDCARD_MISO 2
+#define SDCARD_SS       13
+#define SDCARD_CLK      18
+#define SDCARD_MOSI     23
+#define SDCARD_MISO     2
 
 #include <FS.h>
 
@@ -33,36 +33,39 @@ TaskHandle_t Fill_Buffer_Handle;
 byte Current_Buffer = 1;
 
 File file;
+int flrdsz;
 
-void startULPSound() {
-  pinMode(2, INPUT_PULLUP);
-
+void open_Sound_File_SD() {
   sdSPI.begin(SDCARD_CLK, SDCARD_MISO, SDCARD_MOSI, SDCARD_SS);
-  if (!SD.begin(SDCARD_SS, sdSPI)) {
 
+  if (!SD.begin(SDCARD_SS, sdSPI)) {
     Serial.println(F("Cannot init SD"));
   }
-
   // sample is 11025 Hz 8 bit PCM in .WAV container
   // dowloaded from http://www.nch.com.au/acm/11k8bitpcm.wav
-
   file = SD.open("/11k8bitpcm.wav");
   if (!file) {
     Serial.println(F("cannot open file"));
   }
   file.seek(0);
+}
+
+
+void startULPSound() {
+
+
 
   Serial.println(F("Calc ULP Clock"));
   //calculate the actual ULP clock
   unsigned long rtc_8md256_period = rtc_clk_cal(RTC_CAL_8MD256, 1000);
-  unsigned long rtc_fast_freq_hz = 1000000 ULL * (1 << RTC_CLK_CAL_FRACT) * 256 / rtc_8md256_period;
+  unsigned long rtc_fast_freq_hz = 1000000ULL * (1 << RTC_CLK_CAL_FRACT) * 256 / rtc_8md256_period;
 
   Serial.println(F("Init dac"));
   //initialize DACs
   dac_output_enable(DAC_CHANNEL_1);
-  dac_output_enable(DAC_CHANNEL_2);
+  //dac_output_enable(DAC_CHANNEL_2);
   dac_output_voltage(DAC_CHANNEL_1, 128);
-  dac_output_voltage(DAC_CHANNEL_2, 128);
+  //dac_output_voltage(DAC_CHANNEL_2, 128);
 
   int retAddress1 = 13;
 
@@ -70,53 +73,54 @@ void startULPSound() {
   Serial.print("Real RTC clock: ");
   Serial.println(rtc_fast_freq_hz);
   int dt = (rtc_fast_freq_hz / samplingRate) - loopCycles;
-  if (dt < 0)
+  if (dt < 0) {
     Serial.println("Sampling rate too high");
+  }
   Serial.print("dt: ");
   Serial.println(dt);
   const ulp_insn_t mono[] = {
     //reset offset register
     I_MOVI(R3, 0),
     //delay to get the right sampling rate
-    I_DELAY(dt), // 6 + dt
+    I_DELAY(dt),                // 6 + dt
     //reset sample index
-    I_MOVI(R0, 0), // 6
+    I_MOVI(R0, 0),              // 6
     //write the index back to memory for the main cpu
     I_ST(R0, R3, indexAddress), // 8
     //divide index by two since we store two samples in each dword
-    I_RSHI(R2, R0, 1), // 6
+    I_RSHI(R2, R0, 1),          // 6
     //load the samples
-    I_LD(R1, R2, bufferStart), // 8
+    I_LD(R1, R2, bufferStart),  // 8
     //get if odd or even sample
-    I_ANDI(R2, R0, 1), // 6
+    I_ANDI(R2, R0, 1),          // 6
     //multiply by 8
-    I_LSHI(R2, R2, 3), // 6
+    I_LSHI(R2, R2, 3),          // 6
     //shift the bits to have the right sample in the lower 8 bits
-    I_RSHR(R1, R1, R2), // 6
+    I_RSHR(R1, R1, R2),         // 6
     //mask the lower 8 bits
-    I_ANDI(R1, R1, 255), // 6
+    I_ANDI(R1, R1, 255),        // 6
     //multiply by 2
-    I_LSHI(R1, R1, 1), // 6
+    I_LSHI(R1, R1, 1),          // 6
     //add start position
-    I_ADDI(R1, R1, dacTableStart1), // 6
+    I_ADDI(R1, R1, dacTableStart1),// 6
     //jump to the dac opcode
-    I_BXR(R1), // 4
+    I_BXR(R1),                  // 4
     //here we get back from writing a sample
     //increment the sample index
-    I_ADDI(R0, R0, 1), // 6
+    I_ADDI(R0, R0, 1),          // 6
     //if reached end of the buffer, jump relative to index reset
-    I_BGE(-13, totalSamples), // 4
+    I_BGE(-13, totalSamples),   // 4
     //wait to get the right sample rate (2 cycles more to compensate the index reset)
-    I_DELAY((unsigned int) dt + 2), // 8 + dt
+    I_DELAY((unsigned int)dt + 2),            // 8 + dt
     //if not, jump absolute to where index is written to memory
     I_BXI(3)
-  }; // 4
+  };                  // 4
   // write io and jump back another 12 + 4
 
   Serial.println(F("Load ULP"));
   size_t load_addr = 0;
   size_t size = sizeof(mono) / sizeof(ulp_insn_t);
-  ulp_process_macros_and_load(load_addr, mono, & size);
+  ulp_process_macros_and_load(load_addr, mono, &size);
   //  this is how to get the opcodes
   //  for(int i = 0; i < size; i++)
   //    Serial.println(RTC_SLOW_MEM[i], HEX);
@@ -128,8 +132,9 @@ void startULPSound() {
   }
 
   //set all samples to 128 (silence)
-  for (int i = 0; i < totalSampleWords; i++)
+  for (int i = 0; i < totalSampleWords; i++) {
     RTC_SLOW_MEM[bufferStart + i] = 0x8080;
+  }
 
   //start
   RTC_SLOW_MEM[indexAddress] = 0;
@@ -145,27 +150,34 @@ long currentSample = 0;
 long pos = 0;
 
 unsigned char nextSample() {
-  return (unsigned char)((int) Buffer[Current_Buffer][pos++]); //+128
+  return (unsigned char)((int)Buffer[Current_Buffer][pos++]); //+128
 }
 
 int lastFilledWord = 0;
 
-void Fill_Buffer(void * pvParameters) {
-  Serial.println(F("fill buffer"));
-  (void) pvParameters;
+void Fill_Buffer(void *pvParameters) {
+  //Serial.println(F("fill buffer"));
+
+  (void)pvParameters;
   for (;;) {
     if (Current_Buffer == 1) {
-      file.read((byte * ) Buffer[0], totalSampleWords * 2);
+      flrdsz = file.read((byte *)Buffer[0], totalSampleWords * 2);
     } else {
-      file.read((byte * ) Buffer[1], totalSampleWords * 2);
+      flrdsz = file.read((byte *)Buffer[1], totalSampleWords * 2);
     }
+
+    if (flrdsz == 0) {
+      vTaskDelete(NULL);
+    }
+
+    // Serial.println(flrdsz);
     vTaskSuspend(NULL);
   }
 }
 
-void fillSamples(void * pvParameters) {
-  (void) pvParameters;
-  Serial.println(F("fillSamples"));
+void fillSamples(void *pvParameters) {
+  (void)pvParameters;
+  // Serial.println(F("fillSamples"));
   for (;;) {
     int currentSample = RTC_SLOW_MEM[indexAddress] & 0xffff;
     int currentWord = currentSample >> 1;
@@ -186,21 +198,32 @@ void fillSamples(void * pvParameters) {
       }
     }
     vTaskDelay(pdMS_TO_TICKS(10));
+
+    if (flrdsz == 0) {
+      vTaskDelay(250);
+      dac_output_disable(DAC_CHANNEL_1);
+      vTaskDelete(NULL);
+    }
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.print("Total samples: ");
   Serial.println(totalSamples);
+  open_Sound_File_SD();
   startULPSound();
 
-  xTaskCreatePinnedToCore(Fill_Buffer, "Fill Buffer", 1024 * 6, NULL, 2, & Fill_Buffer_Handle, 1);
+  xTaskCreatePinnedToCore(Fill_Buffer, "Fill Buffer", 1024 * 6, NULL, 2, &Fill_Buffer_Handle, 1);
   Current_Buffer = 0;
   xTaskCreatePinnedToCore(fillSamples, "Fill Sample", 1024 * 6, NULL, 2, NULL, 0);
 
 }
 
 void loop() {
+
+  // deleting loop task?
   vTaskDelete(NULL);
+
 }
